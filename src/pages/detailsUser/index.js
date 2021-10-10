c3b94ccc2a9879ae53b09b1f47bab38f823c86b7
guizaramellaf91 <guizaramellaf91@gmail.com>
2021-10-10 00:39:43 -0300

    new changes

--- src/pages/detailsUser/index.js
+++ src/pages/detailsUser/index.js
@@ -11,7 +11,7 @@ const Page = (u) => {
                 { !u.user.registered ? 
                 <h1>AVISO IMPORTANTE: Você está logado em uma conta temporária, para usar as funcionalidades do sistema, registre sua conta clicando aqui.</h1>
                 : <h1>MEUS DADOS</h1>}
-                <p>Data de Cadastro: <b>{!u.user.registered ? moment().format('DD/MM/yyyy HH:mm:ss', u.user.registered) : 'N/A'}</b></p>
+                <p>Data de Cadastro: <b>{u.user.registered ? moment(u.user.registered).format('DD/MM/yyyy HH:mm:ss') : 'N/A'}</b></p>
                 <p>Nome: <b>{u.user.name ? u.user.name : 'N/A'}</b></p>
                 <p>Login: <b>{u.user.login ? u.user.login : 'N/A'}</b></p>
                 <p>E-mail: <b>{u.user.email ? u.user.email : 'N/A'}</b></p>
